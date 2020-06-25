08e7668dba3a6780a6eb124a2221c8d04e83d974
arbrown110 <arbrown110@gmail.com>
2020-06-25 10:32:49 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,7 @@ class ApplicationController < Sinatra::Base
 
   post '/login' do
     @user = User.find_by(:username => params[:username])
+    
     if @user != nil && @user.password == params[:password]
       session[:user_id] = @user.id
       redirect to '/account'
@@ -22,6 +23,7 @@ class ApplicationController < Sinatra::Base
 
   get '/account' do
     @current_user = User.find_by_id(session[:user_id])
+  
     if @current_user
       erb :account
     else
