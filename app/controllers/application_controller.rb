e13409fb203f58a6c9c41725e9c9fc085630bffd
Caroline <cvrieous@gmail.com>
2020-06-29 17:38:05 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 require_relative '../../config/environment'
 class ApplicationController < Sinatra::Base
   configure do
-    set :views, Proc.new { File.join(root, "../views/") }
+    set :views, proc { File.join(root, '../views/') }
     enable :sessions unless test?
-    set :session_secret, "secret"
+    set :session_secret, 'secret'
   end
 
   get '/' do
@@ -11,17 +13,28 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
+    user = User.find_by(username: params[:username])
 
+    if user && user.password == params[:password]
+      session[:user_id] = user.id
+      redirect '/account'
+    end
+    erb :error
   end
 
   get '/account' do
+    if Helpers.is_logged_in?(session)
+      @user = Helpers.current_user(session)
 
+      erb :account
+    else
+      erb :error
+    end
   end
 
   get '/logout' do
+    session.clear
 
+    redirect '/'
   end
-
-
 end
-
