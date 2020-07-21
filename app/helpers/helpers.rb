eee83e764e7fb5b32791bd394c4b2243c9dcd773
arnie b <arnold.bwaila@gmail.com>
2020-07-21 22:55:44 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,11 +1,10 @@
 class Helpers
-  def current_user(session)
+  def self.current_user(session)
     @user = User.find_by_id(session[:user_id])
     @user
   end
     
-  end
-  def is_logged_in?(session)
-    session[:user_id]? true:false
+  def self.is_logged_in?(session)
+    session[:user_id]? true : false
   end
 end
